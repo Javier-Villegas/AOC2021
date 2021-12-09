5cc3f407917a3775fafb0c655b09c0c6765d54dd
Javier-Villegas <javiervillegascarrasco@gmail.com>
2021-12-09 23:00:54 +0100

    Sadge day9

--- day9.py
+++ day9.py
@@ -13,34 +13,29 @@ def part1(m):
     sel[0,-1] = (m[0,-1] < m[1,-1])&(m[0,-1] < m[0,-2])
     sel[-1,0] = (m[-1,0] < m[-1,1])&(m[-1,0]<m[-2,0])
     sel[-1,-1] = (m[-1,-1] < m[-2,-1])&(m[-1,-1]<m[-1,-2])
-    return (m[sel]+1).sum()
+    return ((m[sel]+1).sum(),sel)
 
-def add_rm_ady(basins,row,col):
+def add_rm_ady(m,checked,row,col):
     res = 1
-    basins[row,col] = False
-    if col > 0 and basins[row,col-1]:
-        res += add_rm_ady(basins,row,col-1)
-    if col < basins.shape[1]-1 and basins[row,col+1]:
-        res += add_rm_ady(basins,row,col+1)
-    if row > 0 and basins[row-1,col]:
-        res += add_rm_ady(basins,row-1,col)
-    if row < basins.shape[0]-1 and basins[row+1,col]:
-        res += add_rm_ady(basins,row+1,col)
+    checked[row,col] = True
+    if col > 0 and m[row,col-1]!=9 and not checked[row,col-1]:
+        res += add_rm_ady(m,checked,row,col-1)
+    if col < m.shape[1]-1 and m[row,col+1]!=9 and not checked[row,col+1]:
+        res += add_rm_ady(m,checked,row,col+1)
+    if row > 0 and m[row-1,col]!=9 and not checked[row-1,col]:
+        res += add_rm_ady(m,checked,row-1,col)
+    if row < m.shape[0]-1 and m[row+1,col]!=9 and not checked[row+1,col]:
+        res += add_rm_ady(m,checked,row+1,col)
     return res
 
-def part2(m):
+def part2(m,pits):
     res = []
-    basins = np.zeros(m.shape).astype(bool)
-    basins[:,1:] |= (m[:,1:] < m[:,:-1])
-    basins[:,:-1] |= (m[:,:-1] < m[:,1:])
-    basins[1:,:] |= (m[1:,:] < m[:-1,:])
-    basins[:-1,:] |= (m[:-1,:] < m[1:,:])
-    print(basins.sum())
 
-    for row in range(basins.shape[0]):
-        for col in range(basins.shape[1]):
-            if basins[row,col]:
-                res.append(add_rm_ady(basins,row,col))
+    checked = np.zeros(m.shape).astype(bool)
+    for row in range(m.shape[0]):
+        for col in range(m.shape[1]):
+            if pits[row,col] and not checked[row,col]:
+                res.append(add_rm_ady(m,checked,row,col))
     print(sorted(res)) 
     return np.multiply.reduce(sorted(res,reverse=True)[0:3])
 if __name__=='__main__':
@@ -50,9 +45,9 @@ if __name__=='__main__':
     with open('inputs/day9.txt') as f:
         m = f.readlines()
     m = np.array(list(map(lambda x: list(x)[:-1],m))).astype(int)
-    res1 = part1(m)
+    (res1,pits) = part1(m)
 
-    res2 = part2(m)
+    res2 = part2(m,pits)
     print(f'Result part 1: {res1}')
     print(f'Result part 2: {res2}')
     end = timer()
